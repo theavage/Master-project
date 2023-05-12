03730d495c012b39649179e4b3a086ac0306e04a
theavage <thea@vage.com>
2023-05-12 13:08:39 +0200

    cleaned

--- network/dataset.py
+++ network/dataset.py
@@ -1,16 +1,28 @@
-import torch
-from torch.utils.data import Dataset, DataLoader
-import numpy as np
-from PIL import Image
+from torch.utils.data import Dataset
 
 class MyDataset(Dataset):
+    """
+    Implements a custom class to use for dataloading with PyTorch dataloader
+    """
+
     def __init__(self, data):
+        """
+        Initializing the data
+        """
+
         self.data = data
         
     def __getitem__(self, index):
+        """
+        Loads and returns a sample from the dataset at the given index 
+        """
+
         x = self.data[index]
         
         return x
     
     def __len__(self):
+        """
+        Returns the number of samples in the dataset
+        """
         return len(self.data)
